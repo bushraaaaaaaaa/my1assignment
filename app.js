b474f490745aaa4e6cb14e59fc995ac04283721b
bushraaaaaaaaa <bushraqadir78@gmail.com>
2023-09-07 03:22:14 +0500

    two files added

---
+++ app.js
@@ -0,0 +1,70 @@
+// chaper 1
+// q1
+// alert("bushra");
+// alert("qadir");
+// alert("bushraqadir78@gmail.com");
+// alert("203284204");
+// alert("pass:sjkdfhs");
+//  q2
+// alert("You're learning JavaScript!");
+// q3
+// alert("hello,how are you");
+// Chapter 2 (Variables for string)
+// q1
+// var firstName;
+// q2
+// var food;
+// food="pizza";
+// q3
+// var teamName;
+// alert(teamName);
+// q4
+// bestMan="Charlie";
+// Chapter 3 (Variables for numbers)
+// q1
+// var caseQty;
+// q2
+// caseQty=144;
+// q3
+// var num=9;
+// q4
+// var number=3;
+// number=3+2; 
+// q5
+// 110
+// q6
+// var flower=4;
+// flower=4+3;
+// Chapter 4 (Variable names Legal and
+//     Illegal)
+// q1
+// var productCost=3.45;
+// q2
+// var nameOfBand;
+// q3
+// var legallyNamed=5;
+// q4
+// var bushraQadir;
+// q5
+// var bushraqadir;(illegal)
+// var bushraQadir;(legal)
+// Chapter 5 (Math Expression I)
+    // q1
+    // modulous
+    // q2
+    // 2
+    // q3
+    // var largeNum = 1000 * 2000; 
+    // q4
+    // var num1=2;
+    // var num2=1;
+    // var res= num1-num2;
+    // q5
+    // var rem=25%6;
+    // /q6
+// var a=2;
+// var b=4;
+// alert(a*b);
+
+
+
